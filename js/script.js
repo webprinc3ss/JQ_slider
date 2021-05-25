a4405947a763e2f31c87647a79a93a84da078644
Mystic Kitty <djabranton@gmail.com>
2021-05-24 20:41:22 -0700

    JQuery coded.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    // alert("Doc is loaded")
+
+    //Set Options
+    let speed = 500; //Fade speed
+    let autoswitch = true; //Auto slider options
+    let autoswitch_speed = 4000; //Auto slider speed
+
+    //Add initial active class
+    $('.slide').first().addClass('active');
+
+    //Hide all slides
+    $('.slide').hide();
+
+    //Show first slide
+    $('.active').show();
+
+    //Next Handler
+    $('#next').on('click', nextSlide);
+
+    //Prev Handler
+    $('#prev').on('click', prevSlide);
+
+    //Auto Slider Handler
+    if (autoswitch == true) {
+        setInterval(nextSlide, autoswitch_speed);
+    }
+    //Switch to the next slide
+    function nextSlide() {
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':last-child')) {
+            $('.slide').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    };
+
+    //Switch to the previous slide
+    function prevSlide() {
+        $('.active').removeClass('active').addClass('oldActive');
+        if ($('.oldActive').is(':first-child')) {
+            $('.slide').last().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+
+});
